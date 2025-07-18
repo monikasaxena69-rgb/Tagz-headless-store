5d7050514d28bb6a6e241f130e655bb39ce1db6d
ZUNOTECH <info@zunotag.com>
2025-07-18 21:54:47 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Zuno - Smart Tracking Technology</title>
-        <meta name="description" content="Never lose anything again with Zuno's smart tracking technology" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   )
 }
