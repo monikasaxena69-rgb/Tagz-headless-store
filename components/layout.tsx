1fd6be64ed7d0a857a08158e8db2da3ee051c8f1
ZUNOTECH <info@zunotag.com>
2025-07-18 21:53:38 +0530

    Create layout.tsx

---
+++ components/layout.tsx
@@ -0,0 +1,14 @@
+import Header from './Header'
+import Footer from './Footer'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-background text-text-light min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
